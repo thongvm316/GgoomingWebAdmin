53ae6b6c7ee3a16a7135d9451d7ada0f8216dcc5
thongvm316 <vominhthong1603@gmail.com>
2021-11-18 15:46:43 +0700

    change url

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://kkoomming.kr/api/ADMIN/v1.0'
-// export const API_URL = 'https://kkoomming.co.kr/api/ADMIN/v1.0/'
+// export const API_URL = 'https://kkoomming.kr/api/ADMIN/v1.0'
+export const API_URL = 'https://kkoomming.co.kr/api/ADMIN/v1.0/'
